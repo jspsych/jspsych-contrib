ada2fc1f60b0bdd706b96227309d96244bf5a9c1
Josh de Leeuw <josh.deleeuw@gmail.com>
2021-09-28 17:41:36 -0400

    add one more test

--- packages/plugin-vsl-animate-occlusion/src/index.spec.ts
+++ packages/plugin-vsl-animate-occlusion/src/index.spec.ts
@@ -2,7 +2,9 @@ import { startTimeline } from "@jspsych/test-utils";
 
 import vslAnimateOcclusion from ".";
 
-jest.useFakeTimers();
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 describe("plugin vsl-animate-occlusion", () => {
   it("should load", async () => {
@@ -14,5 +16,22 @@ describe("plugin vsl-animate-occlusion", () => {
     ]);
 
     await expectRunning();
+  });
+
+  it("should finish automatically", async () => {
+    const { expectRunning, expectFinished } = await startTimeline([
+      {
+        type: vslAnimateOcclusion,
+        stimuli: ["img1.png", "img2.png"],
+        cycle_duration: 100,
+        pre_movement_duration: 0,
+      },
+    ]);
+
+    await expectRunning();
+
+    await timeout(500); // can't use fake timers here because snapsvg uses animation frames.
+
+    await expectFinished();
   });
 });
