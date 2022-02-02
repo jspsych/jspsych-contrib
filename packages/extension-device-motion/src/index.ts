6368846058e9fdc8494cbdf02ac9125db8735ba0
bjoluc <mail@bjoluc.de>
2022-02-02 16:02:53 +0100

    Remove unused MutationObserver logic

--- packages/extension-device-motion/src/index.ts
+++ packages/extension-device-motion/src/index.ts
@@ -1,13 +1,6 @@
 import { JsPsych, JsPsychExtension, JsPsychExtensionInfo } from "jspsych";
 
-interface InitializeParameters {}
-
 interface OnStartParameters {
-  /**
-   * An array of string selectors. The selectors should identify one unique element on the page.
-   * The DOMRect of the element will be stored in the data.
-   */
-  targets?: Array<string>;
   /**
    * An array of mouse events to track. Can include `"mousemove"`, `"mousedown"`, and `"mouseup"`.
    * @default ['devicemotion']
@@ -22,28 +15,15 @@ class DeviceMotionExtension implements JsPsychExtension {
 
   constructor(private jsPsych: JsPsych) {}
 
-  private domObserver: MutationObserver;
   private currentTrialData: Array<object>;
-  private currentTrialTargets: Map<string, DOMRect>;
-  private currentTrialSelectors: Array<string>;
   private currentTrialStartTime: number;
-  private lastSampleTime: number;
   private eventsToTrack: Array<string>;
 
-  initialize = async () => {
-    this.domObserver = new MutationObserver(this.mutationObserverCallback);
-  };
+  initialize = async () => {};
 
-  on_start = (params: OnStartParameters): void => {
-    params = params || {};
-
+  on_start = ({ events = ["devicemotion"] }: OnStartParameters = {}): void => {
     this.currentTrialData = [];
-    this.currentTrialTargets = new Map();
-    this.currentTrialSelectors = params.targets || [];
-    this.lastSampleTime = null;
-    this.eventsToTrack = params.events || ["devicemotion"];
-
-    this.domObserver.observe(this.jsPsych.getDisplayElement(), { childList: true });
+    this.eventsToTrack = events;
   };
 
   on_load = () => {
@@ -57,8 +37,6 @@ class DeviceMotionExtension implements JsPsychExtension {
   };
 
   on_finish = () => {
-    this.domObserver.disconnect();
-
     if (this.eventsToTrack.includes("devicemotion")) {
       window.removeEventListener("devicemotion", this.deviceMotionEventHandler, true);
     }
@@ -72,24 +50,9 @@ class DeviceMotionExtension implements JsPsychExtension {
     const event_time = performance.now();
     const t = Math.round(event_time - this.currentTrialStartTime);
 
-    var x = eventA.acceleration.x;
-    var y = eventA.acceleration.y;
-    var z = eventA.acceleration.z;
-
-    var interval = eventA.interval; //gets interval between samples in ms
-    this.lastSampleTime = event_time;
+    const { x, y, z } = eventA.acceleration;
+    const interval = eventA.interval; //gets interval between samples in ms
     this.currentTrialData.push({ x, y, z, interval, t, event: "devicemotion" });
-  };
-
-  private mutationObserverCallback = (mutationsList, observer) => {
-    for (const selector of this.currentTrialSelectors) {
-      if (!this.currentTrialTargets.has(selector)) {
-        const target = this.jsPsych.getDisplayElement().querySelector(selector);
-        if (target) {
-          this.currentTrialTargets.set(selector, target.getBoundingClientRect());
-        }
-      }
-    }
   };
 }
 
