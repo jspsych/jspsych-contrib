4392f90ad62754a06b864dfd41578e29c0e8087c
Martin Grewe <grewe@zib.de>
2022-10-06 14:50:03 +0200

    module renamed, tracking implemented

--- packages/extension-mediapipe-face-mesh/src/index.ts
+++ packages/extension-mediapipe-face-mesh/src/index.ts
@@ -2,37 +2,48 @@ import "@mediapipe/face_mesh";
 
 import autoBind from "auto-bind";
 import { JsPsych, JsPsychExtension, JsPsychExtensionInfo } from "jspsych";
+import { Euler, Matrix4, Vector3 } from "three";
 
 class MediapipeFacemeshExtension implements JsPsychExtension {
   static info: JsPsychExtensionInfo = {
-    name: "mediapipe-face_mesh",
+    name: "mediapipe-face-mesh",
   };
+
+  private recordedChunks = [];
+  private animationFrameId: number;
+  public mediaStream: MediaStream;
+  private videoElement: HTMLVideoElement;
+  private canvasElement: HTMLCanvasElement;
+  private faceMesh: FaceMesh;
 
   constructor(private jsPsych: JsPsych) {
     autoBind(this);
-    console.log("mediapipe-face_mesh: Constructor");
+  }
 
+  initialize = (): Promise<void> => {
     this.faceMesh = new FaceMesh({
       locateFile: (file) => {
         return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
       },
     });
-  }
 
-  private animationFrameId: number;
-  public mediaStream: MediaStream;
-  private videoElement: HTMLVideoElement;
-  private canvasElement: HTMLCanvasElement;
+    // Set the options for the face mesh tracking
+    this.faceMesh.setOptions({
+      maxNumFaces: 1,
+      // can not be used together with refineLandmarks
+      enableFaceGeometry: true,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+      // can not be used together with enableFaceGeometry
+      refineLandmarks: false,
+    });
 
-  /**
-   * The mediapipe facemesh reference.
-   */
-  private faceMesh: FaceMesh;
-
-  initialize = async () => {};
+    return new Promise((resolve, reject) => {
+      this.faceMesh.initialize().then(resolve, reject);
+    });
+  };
 
   on_start = (): void => {
-    console.log("mediapipe-face_mesh: On start");
     this.canvasElement?.remove();
     this.videoElement?.remove();
 
@@ -48,7 +59,7 @@ class MediapipeFacemeshExtension implements JsPsychExtension {
 
     if (!this.mediaStream) {
       console.warn(
-        "The mediapipe-face_mesh extension is trying to start but the camera is not initialized. Do you need to run the initialize-camera plugin?"
+        "The mediapipe-face-mesh extension is trying to start but the camera is not initialized. Do you need to run the initialize-camera plugin?"
       );
       return;
     }
@@ -61,17 +72,19 @@ class MediapipeFacemeshExtension implements JsPsychExtension {
       this.animationFrameId = window.requestAnimationFrame(this.processFrame.bind(this));
     };
 
+    this.faceMesh.onResults(this.onMediaPipeResult.bind(this));
     this.videoElement.play();
   };
 
   on_load = () => {
-    console.log("mediapipe-face_mesh: On load");
+    this.recordedChunks = [];
   };
 
   on_finish = (): Promise<any> => {
+    console.log("face_mesh tracked chunks: " + this.recordedChunks.length);
     return new Promise((resolve) => {
       this.stopAnimationFrame();
-      resolve({ frame_id: this.animationFrameId });
+      resolve({ face_mesh: this.recordedChunks });
     });
   };
 
@@ -83,15 +96,34 @@ class MediapipeFacemeshExtension implements JsPsychExtension {
   }
 
   private processFrame(): void {
-    console.log("Process frame");
     const canvasContext = this.canvasElement.getContext("2d");
     canvasContext.drawImage(this.videoElement, 0, 0);
 
     // const imageData = canvasContext.getImageData(0, 0, 1280, 720);
 
-    // this.faceMesh.send({ image: this.canvasElement});
+    this.faceMesh.send({ image: this.canvasElement });
 
     this.animationFrameId = window.requestAnimationFrame(this.processFrame.bind(this));
+  }
+
+  private onMediaPipeResult(results: Results): void {
+    if (results.multiFaceGeometry[0] !== undefined) {
+      const transformationMatrix = results.multiFaceGeometry[0]
+        .getPoseTransformMatrix()
+        .getPackedDataList();
+      const rotation = new Euler().setFromRotationMatrix(
+        new Matrix4().fromArray(transformationMatrix)
+      );
+      const translation = new Vector3().setFromMatrixPosition(
+        new Matrix4().fromArray(transformationMatrix)
+      );
+      this.recordedChunks.push({
+        frame_id: this.animationFrameId,
+        transformation: transformationMatrix,
+        rotation: rotation,
+        translation: translation,
+      });
+    }
   }
 }
 
--- packages/extension-mediapipe-face_mesh/src/globals.d.ts
+++
@@ -1,6 +0,0 @@
-import { FaceMesh as MediapipeFaceMesh } from "@mediapipe/face_mesh";
-
-declare global {
-  export type FaceMesh = MediapipeFaceMesh;
-  export const FaceMesh: typeof MediapipeFaceMesh;
-}
