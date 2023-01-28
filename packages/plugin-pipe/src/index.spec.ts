ad1f9da71e67f50f30406cde7e141f1e17758b51
Josh de Leeuw <josh.deleeuw@gmail.com>
2023-01-28 12:00:03 -0500

    remove tests for now

--- packages/plugin-pipe/src/index.spec.ts
+++ packages/plugin-pipe/src/index.spec.ts
@@ -3,9 +3,10 @@ import { startTimeline } from "@jspsych/test-utils";
 import PluginPipe from ".";
 
 jest.useFakeTimers();
-
+/*
 describe("jsPsychPipe.getCondition()", () => {});
 
 describe("jsPsychPipe.saveData()", () => {});
 
 describe("jsPsychPipe.saveBase64Data()", () => {});
+*/
