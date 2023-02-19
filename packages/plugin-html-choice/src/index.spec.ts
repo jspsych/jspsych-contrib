7811ce960f700adbe503e558eb3225964e5631e5
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-02-19 09:12:19 -0500

    Update index.spec.ts

--- packages/plugin-html-choice/src/index.spec.ts
+++ packages/plugin-html-choice/src/index.spec.ts
@@ -6,10 +6,11 @@ jest.useFakeTimers();
 
 describe("html-choice", () => {
   it("should load", async () => {
-    const { expectFinished, getHTML, getData, displayElement, jsPsych } = await startTimeline([
+    const { expectFinished } = await startTimeline([
       {
         type: htmlChoice,
         html_array: [""],
+        trial_duration: 1000,
       },
     ]);
 
