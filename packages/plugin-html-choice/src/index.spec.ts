3fc3dc4638e66be2ff155b4f2ad9d2648f5ac1bd
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-02-19 09:35:39 -0500

    Update index.spec.ts

--- packages/plugin-html-choice/src/index.spec.ts
+++ packages/plugin-html-choice/src/index.spec.ts
@@ -1,3 +1,4 @@
+import htmlMultiResponse from "@jspsych-contrib/plugin-html-multi-response";
 import { clickTarget, startTimeline } from "@jspsych/test-utils";
 
 import htmlChoice from ".";
@@ -5,15 +6,15 @@ import htmlChoice from ".";
 jest.useFakeTimers();
 
 describe("html-choice", () => {
-  it("should load", async () => {
-    const { expectFinished } = await startTimeline([
+  test("displays html", async () => {
+    const { getHTML } = await startTimeline([
       {
         type: htmlChoice,
-        html_array: ["<div>Test-Div</div>"],
+        html_array: ["this is html"],
       },
     ]);
-    clickTarget(document.querySelector("#jspsych-html-choice-0"));
-
-    await expectFinished();
+    expect(getHTML()).toContain(
+      '<div><div class="jspsych-html-choice" id="jspsych-html-choice-0" data-choice="0" value="null">this is html</div></div>'
+    );
   });
 });
