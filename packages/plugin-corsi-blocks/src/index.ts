1d63d22375548adba2a7c3605c302fd5ee0d7cc5
Josh de Leeuw <josh.deleeuw@gmail.com>
2023-03-07 10:20:59 -0500

    change size implementation to be more flexible

--- packages/plugin-corsi-blocks/src/index.ts
+++ packages/plugin-corsi-blocks/src/index.ts
@@ -3,20 +3,35 @@ import { JsPsych, JsPsychPlugin, ParameterType, TrialType } from "jspsych";
 const info = <const>{
   name: "corsi-blocks",
   parameters: {
+    /**
+     * An array of block indexes that specify the order of the sequence to be displayed. For example,
+     * [0, 1, 2, 3, 4] would display the first 5 blocks in the order they appear in the blocks parameter.
+     */
     sequence: {
       type: ParameterType.INT,
       default: undefined,
       array: true,
     },
+    /**
+     * An array of objects that specify the x and y coordinates of each block. The coordinates represent the center
+     * of the block. The coordinates are specified as percentages of the width and height of the display. For example,
+     * {x: 50, y: 50} would place the block in the center of the display.
+     *
+     * The default value is an array of nine blocks that approximates the layout of the original Corsi blocks task.
+     */
     blocks: {
       type: ParameterType.COMPLEX,
       array: true,
       default: [
-        { x: 0, y: 0 },
-        { x: 10, y: 10 },
-        { x: 20, y: 20 },
-        { x: 30, y: 30 },
-        { x: 40, y: 40 },
+        { y: 80, x: 45 },
+        { y: 94, x: 80 },
+        { y: 70, x: 20 },
+        { y: 60, x: 70 },
+        { y: 50, x: 35 },
+        { y: 40, x: 6 },
+        { y: 45, x: 94 },
+        { y: 25, x: 60 },
+        { y: 6, x: 47 },
       ],
       nested: {
         x: {
@@ -29,55 +44,96 @@ const info = <const>{
         },
       },
     },
+    /**
+     * The size of the blocks as a percentage of the overall display size.
+     */
     block_size: {
       type: ParameterType.INT,
-      default: 35,
+      default: 12,
     },
-    arena_width: {
-      type: ParameterType.INT,
-      default: 400,
+    /**
+     * The width of the display, specified as a valid CSS measurement.
+     */
+    display_width: {
+      type: ParameterType.STRING,
+      default: "400px",
     },
-    arena_height: {
-      type: ParameterType.INT,
-      default: 400,
+    /**
+     * The height of the display, specified as a valid CSS measurement.
+     */
+    display_height: {
+      type: ParameterType.STRING,
+      default: "400px",
     },
+    /**
+     * An optional text prompt that can be shown below the display area.
+     */
     prompt: {
       type: ParameterType.STRING,
       default: null,
     },
+    /**
+     * The mode of the trial. If 'display', then the sequence is displayed and the trial ends after
+     * the sequence is complete. If 'input', then the use must click on the blocks in the correct order.
+     */
     mode: {
       type: ParameterType.STRING,
       default: "display",
       options: ["display", "input"],
     },
-    sequence_iti: {
+    /**
+     * The duration, in milliseconds, between each block in the sequence.
+     */
+    sequence_gap_duration: {
       type: ParameterType.INT,
       default: 250,
     },
-    sequence_duration: {
+    /**
+     * The duration, in milliseconds, that each block is displayed during the sequence.
+     */
+    sequence_block_duration: {
       type: ParameterType.INT,
       default: 1000,
     },
+    /**
+     * The duration, in milliseconds, to show the blocks before the sequence begins.
+     */
     pre_stim_duration: {
       type: ParameterType.INT,
       default: 500,
     },
+    /**
+     * The duration, in milliseconds, to show the feedback response animation
+     * during input mode.
+     */
     response_animation_duration: {
       type: ParameterType.INT,
       default: 500,
     },
+    /**
+     * The color of unselected, unhighlighted blocks.
+     */
     block_color: {
       type: ParameterType.STRING,
       default: "#555",
     },
+    /**
+     * The color of the highlighted block.
+     */
     highlight_color: {
       type: ParameterType.STRING,
       default: "#ff0000",
     },
+    /**
+     * The color of correct feedback.
+     */
     correct_color: {
       type: ParameterType.STRING,
       default: "#00ff00",
     },
+    /**
+     * The color of incorrect feedback.
+     */
     incorrect_color: {
       type: ParameterType.STRING,
       default: "#ff0000",
@@ -104,19 +160,20 @@ class CorsiBlocksPlugin implements JsPsychPlugin<Info> {
     let css = `<style id="jspsych-corsi-css">
       #jspsych-corsi-stimulus { 
         position: relative; 
-        width:${trial.arena_width}px; 
-        height:${trial.arena_height}px;
+        width:${trial.display_width}; 
+        height:${trial.display_height};
       }
       .jspsych-corsi-block { 
         background-color: ${trial.block_color}; 
         position: absolute; 
-        width: ${trial.block_size}px; 
-        height: ${trial.block_size}px;
+        width: ${trial.block_size}%; 
+        height: ${trial.block_size}%;
+        transform: translate(-50%, -50%);
       }
       #jspsych-corsi-prompt { 
         position: absolute; 
         text-align: center; 
-        width: trial.arena_width px; 
+        width: ${trial.display_width}; 
         top: 100%; 
       }
       #jspsych-corsi-prompt p { 
@@ -129,11 +186,7 @@ class CorsiBlocksPlugin implements JsPsychPlugin<Info> {
     html += '<div id="jspsych-corsi-stimulus">';
 
     for (let i = 0; i < trial.blocks.length; i++) {
-      html += `<div class="jspsych-corsi-block" data-id="${i}" style="top:calc(${
-        trial.blocks[i].y
-      }% - ${trial.block_size / 2}px); left:calc(${trial.blocks[i].x}% - ${
-        trial.block_size / 2
-      }px);"></div>`;
+      html += `<div class="jspsych-corsi-block" data-id="${i}" style="top:${trial.blocks[i].y}%; left:${trial.blocks[i].x}%;"></div>`;
     }
 
     if (trial.prompt != null) {
@@ -143,9 +196,12 @@ class CorsiBlocksPlugin implements JsPsychPlugin<Info> {
 
     display_element.innerHTML = html;
 
+    const start_time = performance.now();
+
     const trial_data = {
       sequence: trial.sequence,
       response: [],
+      rt: [],
       blocks: trial.blocks,
       correct: null,
     };
@@ -183,7 +239,7 @@ class CorsiBlocksPlugin implements JsPsychPlugin<Info> {
           );
           if (sequence_location < trial.sequence.length) {
             block.style.backgroundColor = trial.highlight_color;
-            wait(update_display, trial.sequence_duration);
+            wait(update_display, trial.sequence_block_duration);
             display_phase = "iti";
           }
           if (sequence_location == trial.sequence.length) {
@@ -195,7 +251,7 @@ class CorsiBlocksPlugin implements JsPsychPlugin<Info> {
           );
           block.style.backgroundColor = trial.block_color;
           sequence_location++;
-          wait(update_display, trial.sequence_iti);
+          wait(update_display, trial.sequence_gap_duration);
           display_phase = "sequence";
         }
       };
@@ -225,7 +281,9 @@ class CorsiBlocksPlugin implements JsPsychPlugin<Info> {
         if (trial_data.correct !== null) {
           return; // extra click during timeout, do nothing
         }
+        const rt = Math.round(performance.now() - start_time);
         trial_data.response.push(parseInt(id));
+        trial_data.rt.push(rt);
         const correct = parseInt(id) == trial.sequence[trial_data.response.length - 1];
         if (correct) {
           display_element
