5afea90da5ecf71043a196683615c2cdde3daea2
atakaragoz <atabk@utexas.edu>
2023-12-24 00:26:28 -0600

    Initial commit of html-written-recall

---
+++ packages/plugin-html-written-recall/README.md
@@ -0,0 +1,35 @@
+# html-written-recall
+
+## Overview
+
+This plugin displays a text box for participants to write a response to a prompt. The response is saved as a string. The trial advances when a certain button is pressed (default is `Spacebar`). The idea is that a general survey-text box allows for editing as well as observing prior responses whereas this input mimics verbal free recall. 
+
+## Loading
+
+### In browser
+
+```js
+<script src="https://unpkg.com/@jspsych-contrib/html-written-recall@1.0.0">
+```
+
+### Via NPM
+
+```
+npm install @jspsych-contrib/html-written-recall
+```
+
+```js
+import jsPsychPluginName from '@jspsych-contrib/html-written-recall';
+```
+
+## Compatibility
+
+Please state which jsPsych version your plugin was developed for use with: "jsPsych v7.x.x" or "jsPsych v6.x.x" (give specific version). 
+
+## Documentation
+
+Link to documentation file in /docs folder, or add documentation to the readme.
+
+## Author / Citation
+
+[atakaragoz](https://www.github.com/atakaragoz)
---
+++ packages/plugin-html-written-recall/index.js
@@ -0,0 +1,149 @@
+var htmlWrittenRecall = (function (jspsych) {
+  "use strict";
+
+  const info = {
+    name: "html-written-recall",
+    parameters: {
+      stimulus: {
+        type: jspsych.ParameterType.HTML_STRING,
+        pretty_name: "Stimulus",
+        default: undefined,
+      },
+      prompt: {
+        type: jspsych.ParameterType.HTML_STRING,
+        pretty_name: "Prompt",
+        default: null,
+      },
+      stimulus_duration: {
+        type: jspsych.ParameterType.INT,
+        pretty_name: "Stimulus duration",
+        default: null,
+      },
+      trial_duration: {
+        type: jspsych.ParameterType.INT,
+        pretty_name: "Trial duration",
+        default: null,
+      },
+      next_word_key: {
+        type: jspsych.ParameterType.STRING,
+        pretty_name: "key for next word",
+        default: "Space",
+      },
+      button_string: {
+        type: jspsych.ParameterType.HTML_STRING,
+        pretty_name: "Button HTML",
+        default: null,
+      },
+      button_delay: {
+        type: jspsych.ParameterType.INT,
+        pretty_name: "Button delay",
+        default: 0,
+      },
+      block_time_start: {
+        type: jspsych.ParameterType.INT,
+        pretty_name: "The time the recall block began",
+        default: null,
+      },
+    },
+  };
+
+  class HtmlFreeRecallPlugin {
+    constructor(jsPsych) {
+      this.jsPsych = jsPsych;
+    }
+
+    trial(display_element, trial) {
+      var new_html =
+        '<div id="jspsych-html-keyboard-response-stimulus">' + trial.stimulus + "</div>";
+
+      // Add a textbox for input
+      new_html +=
+        '<div><input type="text" id="jspsych-html-keyboard-response-textbox" autocomplete="off" /></div>'; // need to make sure autocomplete is off
+
+      if (trial.prompt !== null) {
+        new_html += trial.prompt;
+      }
+
+      if (trial.button_string !== null) {
+        new_html +=
+          '<div><button id="jspsych-html-keyboard-response-button" style="display: none; justify-content:center">' +
+          trial.button_string +
+          "</button></div>";
+      }
+
+      display_element.innerHTML = new_html;
+
+      // Focus the textbox to enable typing immediately
+      var textbox = display_element.querySelector("#jspsych-html-keyboard-response-textbox");
+      textbox.focus();
+      var response = {
+        rt: null,
+        button: null,
+      };
+      // Add an event listener to the button, if it exists, and show it after a delay
+      if (trial.button_string !== null) {
+        var button = display_element.querySelector("#jspsych-html-keyboard-response-button");
+        button.addEventListener("click", () => {
+          response.button = "pressed"; // Set button to "pressed" when clicked
+          end_trial();
+        });
+        var buttonDisplayTime;
+        if (typeof trial.blockTimeStart !== "undefined") {
+          // Dynamic button delay calculation
+          var currentTime = performance.now();
+          var elapsedTime = currentTime - trial.blockTimeStart;
+          buttonDisplayTime = Math.max(trial.totalBlockDuration - elapsedTime, 0);
+        } else {
+          // Static button delay
+          buttonDisplayTime = trial.buttonDelay;
+        }
+        this.jsPsych.pluginAPI.setTimeout(() => {
+          button.style.display = "initial"; // Show the button after the delay
+        }, buttonDisplayTime);
+      }
+      const end_trial = () => {
+        this.jsPsych.pluginAPI.clearAllTimeouts();
+
+        textbox.removeEventListener("keydown", checkForNextWordKey); // Remove the event listener
+
+        var trial_data = {
+          rt: response.rt,
+          stimulus: trial.stimulus,
+          response: textbox.value, // Get the value of the textbox
+          button: response.button,
+        };
+
+        display_element.innerHTML = "";
+
+        this.jsPsych.finishTrial(trial_data);
+      };
+
+      // Function to check for space key press in the textbox
+      const checkForNextWordKey = (event) => {
+        if (textbox.value.trim() !== "" && event.key === trial.next_word_key) {
+          response.rt = performance.now() - start_time;
+          end_trial();
+        }
+      };
+
+      // Add event listener to the textbox for "keydown" event
+      textbox.addEventListener("keydown", checkForNextWordKey);
+
+      if (trial.stimulus_duration !== null) {
+        this.jsPsych.pluginAPI.setTimeout(() => {
+          display_element.querySelector(
+            "#jspsych-html-keyboard-response-stimulus",
+          ).style.visibility = "hidden";
+        }, trial.stimulus_duration);
+      }
+
+      if (trial.trial_duration !== null) {
+        this.jsPsych.pluginAPI.setTimeout(end_trial, trial.trial_duration);
+      }
+      var start_time = performance.now();
+    }
+  }
+  HtmlFreeRecallPlugin.info = info;
+
+  return HtmlFreeRecallPlugin;
+})(jsPsychModule);
