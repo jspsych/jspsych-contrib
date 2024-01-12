46cf7d417b35178a2fc3568d98c71656dfc8d9f4
Josh de Leeuw <josh.deleeuw@gmail.com>
2024-01-12 13:53:20 -0500

    Add html-keyboard-response-raf plugin

---
+++ packages/plugin-html-keyboard-response-raf/docs/html-keyboard-response-raf.md
@@ -0,0 +1,68 @@
+# html-keyboard-response-raf
+
+This plugin implements the same functionality as the html-keyboard-response plugin, but uses requestAnimationFrame internally for timing
+
+## Parameters
+
+In addition to the [parameters available in all plugins](https://jspsych.org/latest/overview/plugins.md#parameters-available-in-all-plugins), this plugin accepts the following parameters. Parameters with a default value of undefined must be specified. Other parameters can be left unspecified if the default value is acceptable.
+
+| Parameter           | Type             | Default Value      | Description                              |
+| ------------------- | ---------------- | ------------------ | ---------------------------------------- |
+| stimulus            | HTML string      | *undefined*        | The string to be displayed.              |
+| choices             | array of strings | `"ALL_KEYS"` | This array contains the key(s) that the participant is allowed to press in order to respond to the stimulus. Keys should be specified as characters (e.g., `'a'`, `'q'`, `' '`, `'Enter'`, `'ArrowDown'`) - see [this page](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values) and [this page (event.key column)](https://www.freecodecamp.org/news/javascript-keycode-list-keypress-event-key-codes/) for more examples. Any key presses that are not listed in the array will be ignored. The default value of `"ALL_KEYS"` means that all keys will be accepted as valid responses. Specifying `"NO_KEYS"` will mean that no responses are allowed. |
+| prompt              | string           | null               | This string can contain HTML markup. Any content here will be displayed below the stimulus. The intention is that it can be used to provide a reminder about the action the participant is supposed to take (e.g., which key to press). |
+| stimulus_duration   | numeric          | null               | How long to display the stimulus in milliseconds. The visibility CSS property of the stimulus will be set to `hidden` after this time has elapsed. If this is null, then the stimulus will remain visible until the trial ends. |
+| trial_duration      | numeric          | null               | How long to wait for the participant to make a response before ending the trial in milliseconds. If the participant fails to make a response before this timer is reached, the participant's response will be recorded as null for the trial and the trial will end. If the value of this parameter is null, then the trial will wait for a response indefinitely. |
+| response_ends_trial | boolean          | true               | If true, then the trial will end whenever the participant makes a response (assuming they make their response before the cutoff specified by the `trial_duration` parameter). If false, then the trial will continue until the value for `trial_duration` is reached. You can set this parameter to `false` to force the participant to view a stimulus for a fixed amount of time, even if they respond before the time is complete. |
+## Data Generated
+
+In addition to the [default data collected by all plugins](https://jspsych.org/latest/overview/plugins.md#data-collected-by-all-plugins), this plugin collects the following data for each trial.
+
+| Name      | Type    | Value                                    |
+| --------- | ------- | ---------------------------------------- |
+| response  | string  | Indicates which key the participant pressed. |
+| rt        | numeric | The response time in milliseconds for the participant to make a response. The time is measured from when the stimulus first appears on the screen until the participant's response. |
+| stimulus  | string  | The HTML content that was displayed on the screen. |
+
+## Install
+
+Using the CDN-hosted JavaScript file:
+
+```js
+<script src="https://unpkg.com/@jspsych-contrib/plugin-html-keyboard-response-raf"></script>
+```
+
+Using the JavaScript file downloaded from a GitHub release dist archive:
+
+```js
+<script src="jspsych/plugin-html-keyboard-response-raf.js"></script>
+```
+
+Using NPM:
+
+```
+npm install @jspsych-contrib/plugin-html-keyboard-response-raf
+```
+
+```js
+import HtmlKeyboardResponseRaf from '@jspsych-contrib/plugin-html-keyboard-response-raf';
+```
+
+## Examples
+
+### Flicker a one-frame stimulus
+
+```javascript
+const trial = {
+  type: jsPsychHtmlKeyboardResponseRaf,
+  stimulus: 'Hello world!',
+  stimulus_duration: 16.6,
+  trial_duration: 50,
+  choices: ['a', 'b', 'c'],
+}
+
+const loop = {
+  timeline: [trial],
+  loop_function: () => true,
+}
+```
---
+++ packages/plugin-html-keyboard-response-raf/src/index.ts
@@ -0,0 +1,228 @@
+import { JsPsych, JsPsychPlugin, ParameterType, TrialType } from "jspsych";
+
+const info = <const>{
+  name: "html-keyboard-response-raf",
+  parameters: {
+    /**
+     * The HTML string to be displayed.
+     */
+    stimulus: {
+      type: ParameterType.HTML_STRING,
+      pretty_name: "Stimulus",
+      default: undefined,
+    },
+    /**
+     * Array containing the key(s) the subject is allowed to press to respond to the stimulus.
+     */
+    choices: {
+      type: ParameterType.KEYS,
+      pretty_name: "Choices",
+      default: "ALL_KEYS",
+    },
+    /**
+     * Any content here will be displayed below the stimulus.
+     */
+    prompt: {
+      type: ParameterType.HTML_STRING,
+      pretty_name: "Prompt",
+      default: null,
+    },
+    /**
+     * How long to show the stimulus.
+     */
+    stimulus_duration: {
+      type: ParameterType.INT,
+      pretty_name: "Stimulus duration",
+      default: null,
+    },
+    /**
+     * How long to show trial before it ends.
+     */
+    trial_duration: {
+      type: ParameterType.INT,
+      pretty_name: "Trial duration",
+      default: null,
+    },
+    /**
+     * If true, trial will end when subject makes a response.
+     */
+    response_ends_trial: {
+      type: ParameterType.BOOL,
+      pretty_name: "Response ends trial",
+      default: true,
+    },
+  },
+};
+
+type Info = typeof info;
+
+/**
+ * **html-keyboard-response-raf**
+ *
+ * jsPsych plugin for displaying a stimulus and getting a keyboard response, using requestAnimationFrame for timing.
+ *
+ * @author Josh de Leeuw
+ */
+class HtmlKeyboardResponseRafPlugin implements JsPsychPlugin<Info> {
+  static info = info;
+
+  private keyboardListener;
+  private hideStimulusTime: number = Infinity;
+  private endTrialTime: number = Infinity;
+  private stimulusIsHidden = false;
+  private currAnimationFrameHandler: number;
+
+  constructor(private jsPsych: JsPsych) {}
+
+  trial(display_element: HTMLElement, trial: TrialType<Info>) {
+    var new_html = '<div id="jspsych-html-keyboard-response-stimulus">' + trial.stimulus + "</div>";
+
+    // add prompt
+    if (trial.prompt !== null) {
+      new_html += trial.prompt;
+    }
+
+    // store response
+    var response = {
+      rt: null,
+      key: null,
+    };
+
+    // draw
+    this.currAnimationFrameHandler = requestAnimationFrame(() => {
+      const initialDisplayTime = performance.now();
+
+      display_element.innerHTML = new_html;
+
+      // start the response listener
+      if (trial.choices != "NO_KEYS") {
+        this.keyboardListener = this.jsPsych.pluginAPI.getKeyboardResponse({
+          callback_function: after_response,
+          valid_responses: trial.choices,
+          rt_method: "performance",
+          persist: false,
+          allow_held_key: false,
+        });
+      }
+
+      // hide stimulus if stimulus_duration is set
+      if (trial.stimulus_duration !== null) {
+        this.hideStimulusTime = initialDisplayTime + trial.stimulus_duration;
+      }
+
+      // end trial if trial_duration is set
+      if (trial.trial_duration !== null) {
+        this.endTrialTime = initialDisplayTime + trial.trial_duration;
+      }
+
+      this.currAnimationFrameHandler = requestAnimationFrame(checkForEnd);
+    });
+
+    const checkForEnd = () => {
+      const currTime = performance.now();
+      if (currTime >= this.hideStimulusTime && !this.stimulusIsHidden) {
+        this.stimulusIsHidden = true;
+        display_element.querySelector<HTMLElement>(
+          "#jspsych-html-keyboard-response-stimulus"
+        ).style.visibility = "hidden";
+        console.log(currTime - this.hideStimulusTime);
+      }
+      if (currTime >= this.endTrialTime) {
+        console.log(currTime - this.endTrialTime);
+        end_trial();
+      } else {
+        this.currAnimationFrameHandler = requestAnimationFrame(checkForEnd);
+      }
+    };
+
+    // function to end trial when it is time
+    const end_trial = () => {
+      cancelAnimationFrame(this.currAnimationFrameHandler);
+
+      // kill keyboard listeners
+      if (typeof this.keyboardListener !== "undefined") {
+        this.jsPsych.pluginAPI.cancelKeyboardResponse(this.keyboardListener);
+      }
+
+      // gather the data to store for the trial
+      var trial_data = {
+        rt: response.rt,
+        stimulus: trial.stimulus,
+        response: response.key,
+      };
+
+      // clear the display
+      display_element.innerHTML = "";
+
+      // move on to the next trial
+      this.jsPsych.finishTrial(trial_data);
+    };
+
+    // function to handle responses by the subject
+    const after_response = (info) => {
+      // after a valid response, the stimulus will have the CSS class 'responded'
+      // which can be used to provide visual feedback that a response was recorded
+      display_element.querySelector("#jspsych-html-keyboard-response-stimulus").className +=
+        " responded";
+
+      // only record the first response
+      if (response.key == null) {
+        response = info;
+      }
+
+      if (trial.response_ends_trial) {
+        end_trial();
+      }
+    };
+  }
+
+  simulate(
+    trial: TrialType<Info>,
+    simulation_mode,
+    simulation_options: any,
+    load_callback: () => void
+  ) {
+    if (simulation_mode == "data-only") {
+      load_callback();
+      this.simulate_data_only(trial, simulation_options);
+    }
+    if (simulation_mode == "visual") {
+      this.simulate_visual(trial, simulation_options, load_callback);
+    }
+  }
+
+  private create_simulation_data(trial: TrialType<Info>, simulation_options) {
+    const default_data = {
+      stimulus: trial.stimulus,
+      rt: this.jsPsych.randomization.sampleExGaussian(500, 50, 1 / 150, true),
+      response: this.jsPsych.pluginAPI.getValidKey(trial.choices),
+    };
+
+    const data = this.jsPsych.pluginAPI.mergeSimulationData(default_data, simulation_options);
+
+    this.jsPsych.pluginAPI.ensureSimulationDataConsistency(trial, data);
+
+    return data;
+  }
+
+  private simulate_data_only(trial: TrialType<Info>, simulation_options) {
+    const data = this.create_simulation_data(trial, simulation_options);
+
+    this.jsPsych.finishTrial(data);
+  }
+
+  private simulate_visual(trial: TrialType<Info>, simulation_options, load_callback: () => void) {
+    const data = this.create_simulation_data(trial, simulation_options);
+
+    const display_element = this.jsPsych.getDisplayElement();
+
+    this.trial(display_element, trial);
+    load_callback();
+
+    if (data.rt !== null) {
+      this.jsPsych.pluginAPI.pressKey(data.response, data.rt);
+    }
+  }
+}
+
+export default HtmlKeyboardResponseRafPlugin;
