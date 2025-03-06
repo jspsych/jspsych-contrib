28e49082e42972c7ba6458ce6ecc3d85240eb54d
Cherrie Chang <cchang@vassar.edu>
2025-03-06 11:17:33 -0500

    sync package-lock; delete templates folder; change default description in readme table if not given

--- update-readme.js
+++ update-readme.js
@@ -59,14 +59,15 @@ function updateRootReadme() {
         info.authorUrl != "" ? `[${info.author}](${info.authorUrl})` : info.author;
       pluginList = pluginList.concat(
         `[${pluginName}](${packageReadmeLink}) | ${authorRender} | ${
-          info.description ? info.description : "foo"
+          info.description ? info.description : "_Description for " + pluginName + "._"
         } \n`
       );
     } else {
+      const extensionName = info.name.replace(/^\@jspsych-contrib\/extension-/g, "");
       extensionList = extensionList.concat(
-        `[${info.name.replace(/^\@jspsych-contrib\/extension-/g, "")}](${packageReadmeLink}) | [${
-          info.author
-        }](${info.authorUrl}) | ${info.description ? info.description : "foo"} \n`
+        `[${extensionName}](${packageReadmeLink}) | [${info.author}](${info.authorUrl}) | ${
+          info.description ? info.description : "_Description for " + extensionName + "._"
+        } \n`
       );
     }
   });
