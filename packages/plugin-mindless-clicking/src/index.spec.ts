97beba2460ee5636c76b746e4f19ae0951649e38
Josh de Leeuw <josh.deleeuw@gmail.com>
2025-05-12 23:34:20 -0400

    add second test example

--- packages/plugin-mindless-clicking/src/index.spec.ts
+++ packages/plugin-mindless-clicking/src/index.spec.ts
@@ -1,4 +1,4 @@
-import { startTimeline } from "@jspsych/test-utils";
+import { clickTarget, startTimeline } from "@jspsych/test-utils";
 
 import jsPsychPluginMindlessClicking from ".";
 
@@ -14,5 +14,25 @@ describe("my plugin", () => {
     ]);
 
     await expectFinished();
+  });
+  it("should finish when the button is clicked the required number of times", async () => {
+    const { expectFinished, getHTML, getData, displayElement, jsPsych } = await startTimeline([
+      {
+        type: jsPsychPluginMindlessClicking,
+        required_clicks: 5,
+      },
+    ]);
+
+    const button = displayElement.querySelector("#jspsych-mindless-clicking-button");
+    for (let i = 0; i < 5; i++) {
+      clickTarget(button);
+      jest.advanceTimersByTime(100);
+    }
+
+    await expectFinished();
+
+    const data = getData();
+    expect(data.values()[0].rt).toBeDefined();
+    expect(data.values()[0].clicks).toBe(5);
   });
 });
