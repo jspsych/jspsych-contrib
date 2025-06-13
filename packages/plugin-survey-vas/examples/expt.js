c5419e2c939503d964fb5598ded91fce702cfd1c
Cherrie Chang <cchang@vassar.edu>
2025-06-13 14:18:26 -0400

    revert style changes to two plugin files

--- packages/plugin-survey-vas/examples/expt.js
+++ packages/plugin-survey-vas/examples/expt.js
@@ -6,21 +6,22 @@ var jsPsych = initJsPsych({
 
 var trial = {
   type: jsPsychSurveyVas,
-  preamble: "Answer the following questions",
+  preamble: 'Answer the following questions',
   questions: [
-    { prompt: "Q1", labels: ["a", "b"] },
-    { prompt: "Q2", labels: ["a", "b", "c"] },
-    { prompt: "Q3", labels: ["a", "b"] },
-    { prompt: "Important!", labels: ["a", "b"], required: true },
-    { prompt: "Also Important!", labels: ["a", "b"], required: true },
+    {prompt: 'Q1', labels: ['a', 'b']},
+    {prompt: 'Q2', labels: ['a', 'b', 'c']},
+    {prompt: 'Q3', labels: ['a', 'b']},
+    {prompt: 'Important!', labels: ['a', 'b'], required: true},
+    {prompt: 'Also Important!', labels: ['a', 'b'], required: true}
   ],
   ticks: true,
   hline_pct: 90,
   scale_width: 300,
   // scale_height: 20,
-  marker_type: "cross",
+  marker_type: 'cross',
   randomize_question_order: true,
-  n_scale_points: 5,
-};
+  n_scale_points: 5
+}
+  
 
 jsPsych.run([trial]);
