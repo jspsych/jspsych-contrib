7afdf13bbef604e83aebabe3f9f6b3fd1cd14830
alinen <alinen@savvysine.com>
2026-03-12 14:48:28 -0400

    Support infinite play option with null duration

--- packages/plugin-tangram-game/src/timebar.js
+++ packages/plugin-tangram-game/src/timebar.js
@@ -2,18 +2,23 @@ class TimeBar {
   constructor(svg, svgElement, duration) {
     this.svg = svg;
     this.el = svgElement;
-    this.timeLeft = duration;
+    this.timeLeft = duration != null ? duration : 99999;
     this.duration = duration;
+    this.elapsedTime = 0;
   }
 
   elapsedTime() {
-    return this.duration - this.timeLeft;
+    return this.elapsedTime;
   }
 
   tick(dt) {
-    this.timeLeft = Math.max(0, this.timeLeft - dt);
-    var fraction = this.timeLeft / this.duration;
-    var width = fraction * this.svg.viewBox.baseVal.width;
-    this.el.setAttribute("width", width);
+    this.elapsedTime += dt;
+
+    if (this.duration != null) {
+      this.timeLeft = Math.max(0, this.timeLeft - dt);
+      var fraction = this.timeLeft / this.duration;
+      var width = fraction * this.svg.viewBox.baseVal.width;
+      this.el.setAttribute("width", width);
+    }
   }
 }
