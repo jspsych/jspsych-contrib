1b1dae30e904567ebfd1719dda838b91fed9dc93
jade <101148768+jadeddelta@users.noreply.github.com>
2026-04-23 08:43:07 -0400

    add a quick test and fix up examples

--- packages/plugin-numpad/src/index.spec.ts
+++ packages/plugin-numpad/src/index.spec.ts
@@ -1,19 +1,25 @@
-import { startTimeline } from "@jspsych/test-utils";
+import { clickTarget, startTimeline } from "@jspsych/test-utils";
 
 import jsPsychNumpad from ".";
 
 jest.useFakeTimers();
 
-describe("my plugin", () => {
-  it("should load", async () => {
-    const { expectFinished, getHTML, getData, displayElement, jsPsych } = await startTimeline([
+describe("plugin-numpad", () => {
+  it("records response and rt after pressing 1 then continue", async () => {
+    const { expectFinished, getData } = await startTimeline([
       {
         type: jsPsychNumpad,
-        parameter_name: 1,
-        parameter_name2: "img.png",
       },
     ]);
 
+    clickTarget(document.querySelector(".jspsych-numpad-button-digit[data-digit='1']"));
+    jest.advanceTimersByTime(1000);
+    clickTarget(document.querySelector(".jspsych-numpad-button-continue"));
+
     await expectFinished();
+
+    const data = getData().values()[0];
+    expect(data.response).toBe(1);
+    expect(data.rt).toBeGreaterThan(0);
   });
 });
